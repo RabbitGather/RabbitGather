6d7a0af852d9b8b4fe559d3281901baa3ac5e132
jacky_li <a.meowalien@gmail.com>
2022-10-24 01:05:17 +0800

    add @ path alias

--- frountend/rabbit_gather/src/main.ts
+++ frountend/rabbit_gather/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router' // 引入router
-import store from './store' // 引入store
+import '@/style.css'
+import App from '@/App.vue'
+import router from '@/router' // 引入router
+import store from '@/store' // 引入store
 import 'virtual:windi.css'
 
 createApp(App)
