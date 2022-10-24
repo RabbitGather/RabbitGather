783df9844422aab83ba91b6cecd04b626699a38c
jacky_li <a.meowalien@gmail.com>
2022-10-24 13:33:11 +0800

    change vue router as createWebHistory

--- frountend/rabbit_gather/src/router/index.ts
+++ frountend/rabbit_gather/src/router/index.ts
@@ -1,19 +1,19 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
-import Home from '../App.vue'
+import { createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 // 創建一個 routes 陣列，定義所有 route 路徑
 const routes: Array<RouteRecordRaw> = [
     {
-        path: '/',
-        name: 'Home',
-        component: Home
+        path: '/hello',
+        name: 'HelloWorld',
+        component: HelloWorld
     }
 ]
 
 
 // 創建一個router，
 const router = createRouter({
-    history: createWebHashHistory(), // 這邊我使用 # 路徑模式
+    history: createWebHistory(), // 這邊我使用 # 路徑模式
     routes // 導入使用上方 routes 所定義的路徑
 })
 
