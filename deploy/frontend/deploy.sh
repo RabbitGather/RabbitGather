7ce363a3138af875681d92a40c64379c8085ccfd
jacky_li <jacky_li@paradise-soft.com.tw>
2022-12-30 18:37:36 +0800

    fix deploy

---
+++ deploy/frontend/deploy.sh
@@ -0,0 +1,4 @@
+# shellcheck disable=SC2086
+docker pull ${IMAGE_NAME}
+#docker rm -f frontend
+IMAGE_NAME=${IMAGE_NAME} docker compose up -d frontend
--- tools/deploy/frontend/deploy.sh
+++
@@ -1,6 +0,0 @@
-# shellcheck disable=SC2086
-docker pull ${IMAGE_NAME}
-docker rm -f frontend
-IMAGE_NAME=${IMAGE_NAME} docker compose up -d
-
-#docker run -d -p 80:80 -p 443:443 --name "${CONTAINER_NAME}" --restart=always -v "${MEOWALIEN_PUBLIC_KEY_PATH}":/certs/meowalien.com.crt -v ${MEOWALIEN_PRIVATE_KEY_PATH}:/certs/meowalien.com.key ${IMAGE_NAME}
