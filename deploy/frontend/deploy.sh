9a4c40f66dfb7068d695b39c1c07cf7f7d988e51
jacky_li <a.meowalien@gmail.com>
2022-12-31 16:02:43 +0800

    test cicd

--- deploy/frontend/deploy.sh
+++ deploy/frontend/deploy.sh
@@ -1,5 +1,5 @@
 # shellcheck disable=SC2086
 docker pull ${IMAGE_NAME}
-docker rm -f frontend || true
-IMAGE_NAME=${IMAGE_NAME} CONTAINER_NAME=${CONTAINER_NAME} docker compose up -d frontend
+docker rm -f ${CONTAINER_NAME} || true
+IMAGE_NAME=${IMAGE_NAME} CONTAINER_NAME=${CONTAINER_NAME} docker compose up -d deploy
 
