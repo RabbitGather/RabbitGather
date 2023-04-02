2ba3d2bd9ad6e399fab8394838956b07a26ba444
jacky_li <jacky_li@Jackys-MacBook-Pro.local>
2023-04-02 23:16:17 +0800

    done meowalien.local self signed certificate

--- scripts/self_signed_certificate.sh
+++ scripts/self_signed_certificate.sh
@@ -1,68 +1,51 @@
 #!/bin/bash
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: <command> <hostname> <cert_output_dir>"
-    exit 1
-fi
 
 HOST_NAME=$1
 CERT_DIR=${2:-$(pwd)/certs}
-FULL_CHAIN_NAME="${HOST_NAME}_fullchain.pem"
 
-mkdir -p "${CERT_DIR}"
-
-function create_and_trust_root_cert() {
-    openssl genrsa -aes256 -passout pass:password -out "${CERT_DIR}/root.key" 4096
-
-    openssl req -x509 -new -sha512 -days 365 \
-        -subj "/C=TW/ST=Taipei/L=Taipei/O=test/OU=lab/CN=root" \
-        -passin pass:password \
-        -key "${CERT_DIR}/root.key" \
-        -out "${CERT_DIR}/root.pem"
-    echo "Adding root.pem to keychain, need permission for sudo:"
-    sudo security add-certificates ${CERT_DIR}/root.pem
-    sudo security add-trusted-cert -r trustRoot -d ${CERT_DIR}/root.pem
-}
-
-if ! [ -f "${CERT_DIR}/root.pem" ]; then
-    create_and_trust_root_cert || exist 1
-fi
-
-function generate_certificate() {
-mkdir -p "${CERT_DIR}"
-
-  openssl genrsa -out "${CERT_DIR}/${HOST_NAME}.key" 4096
-
-  openssl req -sha512 -new \
-      -subj "/C=TW/ST=Taiwan/L=Taipei/O=test/OU=lab/CN=*.${HOST_NAME}" \
-      -key "${CERT_DIR}/${HOST_NAME}.key" \
-      -out "${CERT_DIR}/${HOST_NAME}.csr"
-
-  echo "authorityKeyIdentifier=keyid,issuer
-        basicConstraints=CA:FALSE
-        keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign, cRLSign
-        extendedKeyUsage = serverAuth
-        subjectAltName = DNS:${HOST_NAME},DNS:*.${HOST_NAME}" > /tmp/v3.ext
-#  ALTNAMES="DNS:*.local,DNS:local"
-
-#  openssl x509 -req -days 365 -in "${CERT_DIR}/${HOST_NAME}.csr" -signkey "${CERT_DIR}/${HOST_NAME}.key" -out "${CERT_DIR}/${HOST_NAME}.pem"  -extensions v3_req   -extfile <(echo "[v3_req]
-#                                                                                   subjectAltName=$ALTNAMES")
-
-  openssl x509 -req -sha512 -days 365 \
-      -extfile /tmp/v3.ext \
-      -passin pass:password \
-      -CA ${CERT_DIR}/root.pem -CAkey ${CERT_DIR}/root.key -CAcreateserial \
-      -in "${CERT_DIR}/${HOST_NAME}.csr" \
-      -out "${CERT_DIR}/${HOST_NAME}.pem"
-
-
-  cat ${CERT_DIR}/${HOST_NAME}.pem ${CERT_DIR}/root.pem > ${CERT_DIR}/${FULL_CHAIN_NAME}
-
-  sudo security add-certificates ${CERT_DIR}/${FULL_CHAIN_NAME}
-}
-
-# check if directory exists
-if ! [ -d "${CERT_DIR}/${HOST_NAME}.pem" ]; then
-    generate_certificate || exit 1
+if [ -z "$1" ]; then
+    echo "Usage: <command> <hostname>"
+    echo "Usage: <command> <hostname> <cert_output_dir>"
+    echo "if <cert_output_dir> is not specified, it will be default as \"\$(pwd)/certs\" directory"
+    exit 1
 fi
 
 
+openssl genrsa -aes256 -passout pass:password -out "${CERT_DIR}/rootCA.key" 4096
+
+openssl req -x509 -new -nodes -key "${CERT_DIR}/rootCA.key" -sha256 -days 365000 -subj "/C=TW/ST=Taipei/L=Taipei/O=meowalien/OU=meowalien/CN=meowalien" -passin pass:password -out "${CERT_DIR}/rootCA.crt"
+
+sudo security add-certificates "${CERT_DIR}/rootCA.crt"
+sudo security add-trusted-cert -r trustRoot -d "${CERT_DIR}/rootCA.crt"
+
+openssl genrsa -out "${CERT_DIR}/${HOST_NAME}.key" 4096
+
+echo "[req]
+default_bits = 4096
+prompt = no
+default_md = sha512
+distinguished_name = dn
+
+[dn]
+C=TW
+ST=Taiwan
+L=Taipei
+O=meowalien
+OU=meowalien
+emailAddress=meowalien@meowalien.com
+CN=*.${HOST_NAME}" > "${CERT_DIR}/${HOST_NAME}.cnf"
+
+openssl req -new -sha256 -nodes -key "${CERT_DIR}/${HOST_NAME}.key" -out "${CERT_DIR}/${HOST_NAME}.csr" -config "${CERT_DIR}/${HOST_NAME}.cnf"
+echo "authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment, keyAgreement, keyCertSign, cRLSign
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = *.${HOST_NAME}" > "${CERT_DIR}/${HOST_NAME}-v3.ext"
+
+openssl x509 -req -passin pass:password -in "${CERT_DIR}/${HOST_NAME}.csr" -CA "${CERT_DIR}/rootCA.crt" -CAkey "${CERT_DIR}/rootCA.key" -CAcreateserial -out "${CERT_DIR}/${HOST_NAME}.crt" -days 36500 -sha256 -extfile "${CERT_DIR}/${HOST_NAME}-v3.ext"
+
+cat "${CERT_DIR}/${HOST_NAME}.crt" "${CERT_DIR}/rootCA.crt" > "${CERT_DIR}/${HOST_NAME}_fullchain.crt"
+sudo security add-certificates "${CERT_DIR}/${HOST_NAME}_fullchain.crt"
+sudo security add-trusted-cert -r trustRoot -d "${CERT_DIR}/${HOST_NAME}_fullchain.crt"
--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-bash ./scripts/self_signed_certificate.sh "local" "$(pwd)/certs"
-bash ./scripts/add_to_local_dns.sh "127.0.0.1" "dashboard.local"
+bash ./scripts/self_signed_certificate.sh "meowalien.local" "$(pwd)/certs"
 
+bash ./scripts/add_to_local_dns.sh "127.0.0.1" "dashboard.meowalien.local"
+
+docker compose up traefik
